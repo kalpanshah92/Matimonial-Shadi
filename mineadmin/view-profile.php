70cf7e3cbbaa59c869f00bac3359321470636008
Kalpan Shah <kalpanshah@ymail.com>
2026-05-05 22:32:00 +1000

    Fix missing functions.php include in admin profile view

--- mineadmin/view-profile.php
+++ mineadmin/view-profile.php
@@ -2,6 +2,7 @@
 session_start();
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../includes/functions.php';
 
 if (!isset($_SESSION['admin_id'])) {
     header('Location: login.php');
