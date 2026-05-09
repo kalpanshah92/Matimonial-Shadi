8f9c151fe83ff9ca3a5ad7f2825ee1232385eed0
Kalpan Shah <kalpanshah@ymail.com>
2026-05-09 18:47:01 +1000

    Add query to reset all marital_status values to NULL

--- database/migrations/fix_marital_status_default.sql
+++ database/migrations/fix_marital_status_default.sql
@@ -1,9 +1,7 @@
--- Migration: Remove default 'Never Married' on marital_status
--- Set existing users where marital_status was the auto-default to NULL is risky.
--- This only changes the column default for new users.
-
-ALTER TABLE users
-    ALTER COLUMN marital_status DROP DEFAULT;
+-- Migration: Remove default 'Never Married' on marital_status and reset existing values to NULL
 
 ALTER TABLE users
     MODIFY marital_status VARCHAR(30) DEFAULT NULL;
+
+-- Reset all existing marital_status values to NULL so users explicitly choose
+UPDATE users SET marital_status = NULL;
