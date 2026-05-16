1b4c85511e12b8435d24c009fefe30b57fbe7c6c
Kalpan Shah <kalpanshah@ymail.com>
2026-05-16 16:44:38 +1000

    fix(my-profile): pass encoded profile id to profile.php (decodeProfileId requires encoded form)

--- my-profile.php
+++ my-profile.php
@@ -3,4 +3,4 @@ $pageTitle = 'My Profile';
 require_once __DIR__ . '/includes/auth.php';
 
 // Redirect to own profile page
-redirect(SITE_URL . '/profile.php?id=' . $currentUser['id']);
+redirect(SITE_URL . '/profile.php?id=' . encodeProfileId($currentUser['id']));
